3ca07497b26bb60c7d25b952e302398eb6c7c05d
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-02-26 18:41:08 -0500

    Agent: Create infra

---
+++ main.tf
@@ -0,0 +1,6 @@
+# Generated at Thu Feb 26 18:41:07 2026
+resource "google_bigquery_table" "table" {
+  dataset_id          = google_bigquery_dataset.dataset.dataset_id
+  table_id            = "table1"
+  deletion_protection = true
+}
