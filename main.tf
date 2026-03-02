1f2cdc1bd496aaa13d3a78e0de35d7f123164062
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-02 17:04:27 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -1,17 +1,28 @@
-resource google_cloud_run_v2_service CloudRunApi {
-  name     = cloud_run_api
-  location = us-central1
+resource "google_storage_bucket" "bucket" {
+  name          = "clouyd_storage_bucket"
+  location      = "US"
+  storage_class = "STANDARD"
+  force_destroy = false 
+  
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "google_cloud_run_v2_service" "service" {
+  name     = "cloud_run_service"
+  location = "us-central1"
 
   template {
     scaling {
       max_instance_count = 10
     }
     containers {
-      image = gcr.io/project/image:tag
+      image = "gcr.io/project/image:tag"
       resources {
         limits = {
-          cpu    = 1000m
-          memory = 512Mi
+          cpu    = "1000m"
+          memory = "512Mi"
         }
       }
     }
@@ -20,4 +31,11 @@ resource google_cloud_run_v2_service CloudRunApi {
   lifecycle {
     prevent_destroy = true
   }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "invoker" {
+  location = google_cloud_run_v2_service.service.location
+  name     = google_cloud_run_v2_service.service.name
+  role     = "roles/run.invoker"
+  member   = "allAuthenticatedUsers"
 }
