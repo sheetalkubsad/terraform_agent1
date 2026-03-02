ddaa3fd3d31af3ffbecc0738d630828e2a983c66
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-02 19:25:47 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -21,3 +21,26 @@ resource "google_cloud_run_v2_service" "CloudRunApi" {
     prevent_destroy = true
   }
 }
+resource "google_cloud_run_v2_service" "service" {
+  name     = "cloud_run_api2"
+  location = "us-central1"
+
+  template {
+    scaling {
+      max_instance_count = 10
+    }
+    containers {
+      image = "gcr.io/project/image1:tag"
+      resources {
+        limits = {
+          cpu    = "1000m"
+          memory = "512Mi"
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
