585009f471c3e9388be8f6e2d381ae1e4468d1aa
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-03 16:48:42 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -51,3 +51,11 @@ resource "google_bigquery_dataset" "dataset" {
   default_table_expiration_ms = 7200000
   delete_contents_on_destroy  = false
 }
+
+
+resource "google_cloud_run_v2_service_iam_member" "CloudRunInvokerPolicy" {
+  location = google_cloud_run_v2_service.CloudRunApi.location
+  name     = google_cloud_run_v2_service.CloudRunApi.name
+  role     = "roles/run.invoker"
+  member   = "allAuthenticatedUsers"
+}
