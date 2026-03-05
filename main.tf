c4168857b0f3c142b13a17e696a5a0adb60cdccf
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-05 18:52:01 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -13,3 +13,22 @@ provider "google" {
   region  = "us-central1"
 }
 
+
+module "CloudRunService1" {
+  source = "git::https://github.com/sheetalkubsad/terraform-cloud-run-module.git?ref=main"
+
+  serviceName      = "cloudRunService1"
+  containerImage   = "gcr.io/project/image:tag"
+  maxInstanceCount = 10
+  cpuLimit         = "1000m"
+  memoryLimit      = "512Mi"
+}
+
+module "CloudRunIamPolicy1" {
+  source = "git::https://github.com/sheetalkubsad/terraform-cloud-run-iam-module.git?ref=main"
+
+  service_name = "cloudRunService1"
+  region       = "us-central1"
+  member       = "serviceAccount:my-sa@project.iam.gserviceaccount.com"
+  environment  = "dev"
+}
