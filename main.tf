4b892f4b9f19c92e7562620f6675da91e04fe9c2
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-05 16:13:06 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -1,25 +1,16 @@
-
 terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-  }
 }
 
 provider "google" {
   project = "durable-fact-489014-s6"
-  region  = "us-central1"
+  region = "us-central1"
 }
-
 
 module "Service1" {
   source = "git::https://github.com/sheetalkubsad/terraform-cloud-run-module.git?ref=main"
-
-  serviceName      = "service1"
-  containerImage   = "gcr.io/project/image:tag"
-  maxInstanceCount = 10
-  cpuLimit         = "1000m"
-  memoryLimit      = "512Mi"
+  serviceName = "service1"
+  containerImage = "gcr.io/project/image:tag"
+  maxInstanceCount = 20
+  cpuLimit = "1000m"
+  memoryLimit = "512Mi"
 }
