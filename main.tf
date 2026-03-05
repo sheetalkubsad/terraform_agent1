ed7214f218e938716f9bb3848b3646aee1f00793
sheetalkubsad <kubsadsheetal@gmail.com>
2026-03-05 13:25:47 -0500

    Updated main.tf manually

--- main.tf
+++ main.tf
@@ -13,13 +13,3 @@ provider "google" {
   region  = "us-central1"
 }
 
-
-module "Service1" {
-  source = "git::https://github.com/sheetalkubsad/terraform-cloud-run-module.git?ref=main"
-
-  serviceName      = "service1"
-  containerImage   = "gcr.io/project/image:tag"
-  maxInstanceCount = 10
-  cpuLimit         = "1000m"
-  memoryLimit      = "512Mi"
-}
