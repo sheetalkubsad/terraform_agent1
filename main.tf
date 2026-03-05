f5172618f7d18b9fbefcf33c63456ad16121879b
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-05 18:18:43 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -15,11 +15,10 @@ provider "google" {
 
 
 module "Service1" {
-  source = "git::https://github.com/sheetalkubsad/terraform-cloud-run-module.git?ref=main"
-
+  source           = "git::https://github.com/sheetalkubsad/terraform-cloud-run-module.git?ref=main"
   serviceName      = "service1"
   containerImage   = "gcr.io/project/image:tag"
-  maxInstanceCount = 10
+  maxInstanceCount = 20
   cpuLimit         = "1000m"
   memoryLimit      = "512Mi"
 }
