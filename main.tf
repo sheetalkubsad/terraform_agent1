26bb4b5cd7a12bc7341b1f46faa62aeef73c3e2b
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-05 22:21:20 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -13,3 +13,9 @@ provider "google" {
   region  = "us-central1"
 }
 
+
+resource "google_bigquery_dataset" "BigqueryDataset" {
+  dataset_id                  = "dataset1"
+  location                    = "us-central1"
+  default_table_expiration_ms = 3600000
+}
