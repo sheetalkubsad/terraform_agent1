30ba5ebb6fe04e5746700ff76958dd768cddf876
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-09 17:27:59 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -23,3 +23,12 @@ module "cloudrunService1" {
   cpuLimit         = "1000m"
   memoryLimit      = "512Mi"
 }
+
+module "BigqueryDataset5" {
+  source = "git::https://github.com/sheetalkubsad/terraform-bigquery-module.git?ref=main"
+
+  datasetId         = "dataset5"
+  dataLocation      = "US"
+  allowUpdate       = false
+  tableExpirationMs = 3600000
+}
