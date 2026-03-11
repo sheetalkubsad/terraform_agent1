23e21f80dade645c36bee1df1eed2561901041be
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-11 20:49:21 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -32,3 +32,22 @@ module "CloudRunServiceIamPolicy" {
   member       = "user:test-user@exampl.com"
   environment  = "Development"
 }
+
+module "Dataset5" {
+  source = "git::https://github.com/sheetalkubsad/terraform-bigquery-module.git?ref=main"
+
+  datasetId         = "dataset5"
+  dataLocation      = "US"
+  allowUpdate       = false
+  tableExpirationMs = 3600000
+}
+
+module "CloudRunApi2" {
+  source = "git::https://github.com/sheetalkubsad/terraform-cloud-run-module.git?ref=main"
+
+  serviceName      = "cloud_run_api2"
+  containerImage   = "gcr.io/myproject/api:v2.1"
+  maxInstanceCount = 10
+  cpuLimit         = "1000m"
+  memoryLimit      = "512Mi"
+}
