9473fd86b175c472194c45bf7f814e55f9c022e0
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-11 15:28:41 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -32,3 +32,13 @@ module "CloudRunServiceIamPolicy" {
   member       = "user:test-user@exampl.com"
   environment  = "Development"
 }
+
+module "CloudRunServiceCloudRunApi" {
+  source = "git::https://github.com/sheetalkubsad/terraform-cloud-run-module.git?ref=main"
+
+  serviceName      = "cloud_run_api"
+  containerImage   = "gcr.io/myproject/api:v2.1"
+  maxInstanceCount = 10
+  cpuLimit         = "1000m"
+  memoryLimit      = "512Mi"
+}
