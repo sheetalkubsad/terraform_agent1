f9d6fafb06b9caf9accb238c4555cae2652d2d12
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-03-12 22:38:18 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -32,3 +32,12 @@ module "CloudRunServiceIamPolicy" {
   member       = "user:test-user@exampl.com"
   environment  = "Development"
 }
+
+module "Dataset6" {
+  source = "git::https://github.com/sheetalkubsad/terraform-bigquery-module.git?ref=main"
+
+  datasetId         = "dataset6"
+  dataLocation      = "US"
+  allowUpdate       = false
+  tableExpirationMs = 3600000
+}
