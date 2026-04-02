ae278486c4c61895343b83f40079863ea964e476
sheetalkubsad <48878915+sheetalkubsad@users.noreply.github.com>
2026-04-02 19:58:44 +0000

    Terraform infra update

--- main.tf
+++ main.tf
@@ -32,3 +32,13 @@ module "CloudRunServiceIamPolicy" {
   member       = "user:test-user@exampl.com"
   environment  = "Development"
 }
+
+module "Serd" {
+  source = "git::https://github.com/sheetalkubsad/terraform-cloud-run-module.git?ref=main"
+
+  serviceName      = "serd"
+  containerImage   = "gcr.io"
+  maxInstanceCount = 10
+  cpuLimit         = "1000m"
+  memoryLimit      = "512Mi"
+}
